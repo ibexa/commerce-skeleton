5c2a609112717812d48f49ecc36832e870f7e027
Ibexa Jenkins <jenkins@ibexa.co>
2021-07-12 13:16:45 +0000

    Template for v3.3.6

---
+++ public/assets/ezplatform/build/ezcommerce-business-design-confirmation-page-js.js
@@ -0,0 +1,31 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["ezcommerce-business-design-confirmation-page-js"],{
+
+/***/ "./vendor/ezsystems/ezcommerce-checkout/src/bundle/Resources/public/js/design/confirmation.page.js":
+/*!*********************************************************************************************************!*\
+  !*** ./vendor/ezsystems/ezcommerce-checkout/src/bundle/Resources/public/js/design/confirmation.page.js ***!
+  \*********************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+(function (global, doc) {
+  doc.querySelector('.ezcommerce-js--print-page').addEventListener('click', function (event) {
+    event.preventDefault();
+    global.print();
+  });
+})(window, window.document);
+
+/***/ }),
+
+/***/ 94:
+/*!***************************************************************************************************************!*\
+  !*** multi ./vendor/ezsystems/ezcommerce-checkout/src/bundle/Resources/public/js/design/confirmation.page.js ***!
+  \***************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! /home/jenkins/workspace/DXP/Archive_Maker_Commerce/ibexa-commerce/vendor/ezsystems/ezcommerce-checkout/src/bundle/Resources/public/js/design/confirmation.page.js */"./vendor/ezsystems/ezcommerce-checkout/src/bundle/Resources/public/js/design/confirmation.page.js");
+
+
+/***/ })
+
+},[[94,"runtime"]]]);
